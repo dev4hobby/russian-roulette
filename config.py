4617c9c16087d131b32924ac8dc56ba71c97e6af
Seongchuel <aciddust20@gmail.com>
2021-05-26 13:29:37 +0900

    Update, config

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ from os.path import abspath
 ignore_list = [
     __file__,
     abspath('russian_roulette.py'),
+    abspath('config.py'),
     abspath('run.py'),
     abspath('__pycache__'),
+    abspath('README.md'),
+    abspath('.git'),
 ]
+
+last_man_standing = True
