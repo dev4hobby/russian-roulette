ca56bfca31f7d201a199a817f6e88777d3aee958
d3fau1t <aciddust20@gmail.com>
2021-05-27 15:11:30 +0900

    Fix typo
    bullet -> magazine

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from russian_roulette import Gun
 if __name__ == '__main__':
     _g = Gun()
-    print(_g.bullet)
+    print(_g.magazine)
 
     # execute till you satisfying..
     while not _g.broken:
